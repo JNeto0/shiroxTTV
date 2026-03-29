e42c62f340eb1e5db2df1785e6560deecf8a0fae
João Neto <joaofonseca693@gmail.com>
2026-03-29 19:32:50 -0300

    Adição de footer 1

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
-      <body>{children}</body>
+      <body>
+        <div className="page-container">
+          {children}
+          <footer className="footer">
+            <p>Desenvolvido por <a href="https://instagram.com/jneto.01" target="_blank" rel="noopener noreferrer">jneto.01</a></p>
+          </footer>
+        </div>
+      </body>
     </html>
   );
 }
